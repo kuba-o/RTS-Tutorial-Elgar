f6d8ed10811607de9c1521b563482bf23ca44d3a
kuba-o <ostrowicki.kuba@gmail.com>
2019-06-16 19:27:53 +0200

    parts 4 (world objects) and 5 (buildings and basic selection)

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RTS;
 
 public class Player : MonoBehaviour
 {
     public string username;
     public bool isHuman;
 
+    public Hud hud;
+
+    public WorldObject SelectedObject { get; set; }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hud = GetComponentInChildren<Hud>();
     }
 
     // Update is called once per frame
--- Assets/RTS/ResourceManager.cs
+++ Assets/RTS/ResourceManager.cs
@@ -11,6 +11,8 @@ namespace RTS
         public static int ScrollWidth { get { return 15; } }
         public static float MinCameraHeight { get { return 10; } }
         public static float MaxCameraHeight { get { return 40; } }
+        private static Vector3 invalidPosition = new Vector3(-99999, -99999, -99999);
+        public static Vector3 InvalidPosition { get { return invalidPosition; } }
     }
 }
     
