5034e0cbfeccfcf792a0e2609b25b1c511d0ed0c
kuba-o <ostrowicki.kuba@gmail.com>
2019-07-07 16:17:58 +0200

    Part 9 - resources setup

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -10,17 +10,60 @@ public class Player : MonoBehaviour
 
     public Hud hud;
 
+    public int startMoney, startMoneyLimit, startPower, startPowerLimit;
+    private Dictionary<ResourceType, int> resources, resourceLimits;
     public WorldObject SelectedObject { get; set; }
+
+    void Awake()
+    {
+        resources = InitResourceList();
+        resourceLimits = InitResourceList();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         hud = GetComponentInChildren<Hud>();
+        AddStartResourceLimits();
+        AddStartResources();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isHuman)
+        {
+            hud.SetResourceValues(resources, resourceLimits);
+        }
+    }
+
+    private Dictionary<ResourceType, int> InitResourceList()
+    {
+        Dictionary<ResourceType, int> list = new Dictionary<ResourceType, int>();
+        list.Add(ResourceType.Money, 0);
+        list.Add(ResourceType.Power, 0);
+        return list;
+    }
+
+    private void AddStartResourceLimits()
+    {
+        IncrementResourceLimit(ResourceType.Money, startMoneyLimit);
+        IncrementResourceLimit(ResourceType.Power, startPowerLimit);
+    }
+
+    private void AddStartResources()
+    {
+        AddResource(ResourceType.Money, startMoney);
+        AddResource(ResourceType.Power, startPower);
+    }
+
+    public void AddResource(ResourceType type, int amount)
+    {
+        resources[type] += amount;
+    }
+
+    public void IncrementResourceLimit(ResourceType type, int amount)
+    {
+        resourceLimits[type] += amount;
     }
 }
